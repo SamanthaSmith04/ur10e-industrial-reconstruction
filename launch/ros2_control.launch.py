06178abcece5bc48d8bf2fbc872f167c3736f297
Sam Smith <smith.15485@aim-dnc301392.aims.internal>
2026-04-15 14:22:11 -0400

    ur10e support setup

---
+++ launch/ros2_control.launch.py
@@ -0,0 +1,74 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, RegisterEventHandler
+from launch.event_handlers.on_process_exit import OnProcessExit
+from launch.substitutions import LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterValue
+
+from launch_ros.actions import Node
+import yaml
+import os
+
+
+parameters = [
+    {'name': 'robot_description',      'description': 'URDF content',                                 'default': ''},
+    {'name': 'controllers_file',       'description': 'Path to the ros2_control configuration file',  'default': ''},
+]
+
+
+def declare_launch_arguments():
+    return [DeclareLaunchArgument(entry['name'], description=entry['description'], default_value=entry['default']) for entry in parameters]
+
+
+def generate_launch_description():
+    return LaunchDescription(declare_launch_arguments() + [OpaqueFunction(function=launch)])
+
+
+def launch(context, *args, **kwargs):
+    robot_description = ParameterValue(LaunchConfiguration('robot_description'))
+
+    controllers_file = LaunchConfiguration('controllers_file')
+
+    nodes = [
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[{'robot_description': robot_description}, controllers_file],
+            output="both",
+        )
+    ]
+
+    # Load the controllers YAML file
+    with open(controllers_file.perform(context), 'r') as f:
+        controllers = yaml.safe_load(f)
+
+    # Load each controller sequentially
+    spawners = []
+    for key, val in controllers['controller_manager']['ros__parameters'].items():
+        if type(val) is dict and type(val['type']) is str:
+            arguments = [key, "-c", "/controller_manager"]
+            if not val['active']:
+              arguments.append("--inactive")
+
+            spawners.append(
+                Node(
+                    package="controller_manager",
+                    executable='spawner' if os.environ['ROS_DISTRO'] > 'foxy' else 'spawner.py',
+                    arguments=arguments,
+                    name=f'{key}_spawner',
+                )
+            )
+
+    # Add event handlers that make sure that nodes get launched and finished before the next node launches
+    handlers = []
+    for i in range(len(spawners) - 1):
+        handlers.append(
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawners[i],
+                    on_exit=spawners[i + 1]
+                )
+            )
+        )
+
+    # Launch the nodes, handlers, and only the first controller spawner (the handlers will launch the rest in order)
+    return nodes + handlers + [spawners[0]]
